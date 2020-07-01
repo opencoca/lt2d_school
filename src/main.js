d065813ce7ad3a0e4bd6c53da8bf9fea5dd85264
OpenCo <admin@openco.ca>
2020-07-01 15:38:04 -0400

    tesr

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from "vue";
+import PageTitle from "vue-page-title";
 import App from "./App.vue";
 
 Vue.config.productionTip = false;
+
+Vue.use(PageTitle, {
+  suffix: " - Class "
+});
 
 new Vue({
   render: h => h(App)
