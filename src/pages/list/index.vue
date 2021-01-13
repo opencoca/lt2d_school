eb3e21382436197d9e02c8096507d28295f865a9
OpenCo <admin@openco.ca>
2021-01-13 13:48:39 -0500

    mobile cleanup part 1

--- src/pages/list/index.vue
+++ src/pages/list/index.vue
@@ -85,348 +85,345 @@ export default {
 <style>
 
 a {
-  color:#444499;
-  text-decoration:none;
-}
-
-a:hover {
-  color:#333;
-}
-
-#header{
-  position:fixed;
-  top:0;
-  left:0px;
-  height: 50px;
-  width:100%;
-  min-width: 945px;
-  background:#212533;
-  padding:0;
-  z-index: 10000;
-}
-
-#header img{
-  max-width:80%;
-}
-
-#logo {
-  display:inline;
-  position: absolute;
-  left: 0px;
-}
-
-h1 {
-  margin-top: 5px;
-  padding-top: 5px;
-  color:#f1f1f1;
-  font-size: 1.6em;
-}
-
-.modal-container {
-  margin: 0 auto;
-  padding-top: 10px;
-  position: relative;
-  width: 160px;
-}
-
-.modal-container button {
-  display: none;
-  margin: 0 auto;
-  color: #fff;
-  width: 160px;
-  height: 50px;
-  line-height: 50px;
-  background: #446CB3;
-  font-size: 22px;
-  border: 0;
-  border-radius: 3px;
-  box-shadow: 0 5px 5px -5px #333;
-  transition: background 0.3s ease-in;
-}
-
-.modal-container .modal-backdrop {
-  height: 0;
-  width: 0;
-  opacity: 0;
-  overflow: hidden;
-  transition: opacity 0.2s ease-in;
-}
-
-.modal-container #modal-toggle {
-  position: absolute;
-  left: 0;
-  top: 0;
-  height: 100%;
-  width: 100%;
-  margin: 0;
-  opacity: 0;
-  cursor: pointer;
-}
-
-.modal-container #modal-toggle:hover ~ button {
-  background: #1E824C;
-}
-
-.modal-container #modal-toggle:checked {
-  width: 100vw;
-  height: 100vh;
-  position: fixed;
-  left: 0;
-  top: 0;
-  z-index: 9;
-  opacity: 0;
-}
-
-.modal-container #modal-toggle:checked ~ .modal-backdrop {
-  background-color: rgba(0, 0, 0, 0.6);
-  width: 100vw;
-  height: 100vh;
-  position: fixed;
-  left: 0;
-  top: 0;
-  z-index: 9;
-  pointer-events: none;
-  opacity: 1;
-}
-
-.modal-container #modal-toggle:checked ~ .modal-backdrop .modal-content {
-  background-color: #fff;
-  max-width: 400px;
-  width: 100%;
-  height: 280px;
-  padding: 10px 30px;
-  position: absolute;
-  left: calc(50% - 200px);
-  top: 12%;
-  border-radius: 4px;
-  z-index: 999;
-  pointer-events: auto;
-  cursor: auto;
-  box-shadow: 0 3px 7px rgba(0, 0, 0, 0.6);
-}
-@media (max-width: 400px) {
-  .modal-container #modal-toggle:checked ~ .modal-backdrop .modal-content {
-    left: 0;
+    color:#444499;
+    text-decoration:none;
   }
-}
-
-.modal-container
-  #modal-toggle:checked
-  ~ .modal-backdrop
-  .modal-content
-  .modal-close {
-  color: #666;
-  position: absolute;
-  right: 2px;
-  top: 0;
-  padding-top: 7px;
-  background: #fff;
-  font-size: 16px;
-  width: 25px;
-  height: 28px;
-  font-weight: bold;
-  text-align: center;
-  cursor: pointer;
-}
-
-.modal-container
-  #modal-toggle:checked
-  ~ .modal-backdrop
-  .modal-content
-  .modal-close.button {
-  top: initial;
-  bottom: 20px;
-  right: 20px;
-  background: #4CAF50;
-  color: #fff;
-  width: 50px;
-  border-radius: 2px;
-  font-size: 14px;
-  font-weight: normal;
-}
-
-.modal-container
-  #modal-toggle:checked
-  ~ .modal-backdrop
-  .modal-content
-  .modal-close.button:hover {
-  color: #fff;
-  background: #1E824C;
-}
-
-.modal-container
-  #modal-toggle:checked
-  ~ .modal-backdrop
-  .modal-content
-  .modal-close:hover {
-  color: #333;
-}
-
-#snap {
-  border: none;
-  margin: none;
-  padding: none;
-  position: absolute;
-  top: 50px;
-  left: 0px;
-}
-
-#jitsi {
-  border: none;
-  margin: none;
-  padding: none;
-  position: fixed;
-  top: 50px;
-  left: 100vw;
-  min-width: 526px;
-  background-color: #333;
-}
-
-.jitsiOpen {
-  animation-name: jitsiopenAnimation;
-  animation-duration:0.5s;
-  animation-iteration-count: 1;
-  animation-timing-function: ease;
-  animation-fill-mode: forwards;
-}
-
-.jitsiClose {
-  animation-name: jitsicloseAnimation;
-  animation-duration:0.6s;
-  animation-iteration-count: 1;
-  animation-timing-function: ease;
-  animation-fill-mode: forwards;
-}
-
-.jitsiHalf {
-  animation-name: jitsihalfAnimation;
-  animation-duration:0.5s;
-  animation-iteration-count: 1;
-  animation-timing-function: ease;
-  animation-fill-mode: forwards;
-}
-
-@keyframes jitsiopenAnimation {
-  from {
-    left: 100%;
-    width: 0%;
-  }
-  to {
-    left: 0%;
-    width: 100%;
-  }  
-}
-
-@keyframes jitsihalfAnimation {
-  from {
-    left: 0%;
-    width: 100%;
-    
-  }
-  to {
-    left: 50%;
-    width: 50%;
-  }  
-}
-
-@keyframes jitsicloseAnimation {
-  from {
-    left: 50%;
-    width: 50%;
-    
-  }
-  to {
-    left: 100%;
-    width: 100%;
-  }  
-}
-
-.snapOpen {
-  animation-name: snapopenAnimation;
-  animation-duration:0.5s;
-  animation-iteration-count: 1;
-  animation-timing-function: ease;
-  animation-fill-mode: forwards;
   
-}
-
-.snapClose {
-  animation-name: snapcloseAnimation;
-  animation-duration:1s;
-  animation-iteration-count: 1;
-  animation-timing-function: ease;
-  animation-fill-mode: forwards;
-}
-
-.snapHalf {
-  animation-name: snaphalfAnimation;
-  animation-duration:1s;
-  animation-iteration-count: 1;
-  animation-timing-function: ease;
-  animation-fill-mode: forwards;
-}
-
-@keyframes snapcloseAnimation {
-  from {
-    left: 0%;
+  a:hover {
+    color:#333;
+  }
+  
+  #header{
+    position:fixed;
+    top:0;
+    left:0px;
+    height: 50px;
+    width:100%;
+    background:#212533;
+    padding:0;
+    z-index: 10000;
+  }
+  
+  #header img{
+    max-width:80%;
+  }
+  
+  #logo {
+    display:inline;
+    position: absolute;
+    left: 0px;
+  }
+  
+  h1 {
+    margin-top: 5px;
+    padding-top: 5px;
+    color:#f1f1f1;
+    font-size: 1.6em;
+  }
+  
+  .modal-container {
+    margin: 0 auto;
+    padding-top: 10px;
+    position: relative;
+    width: 160px;
+  }
+  
+  .modal-container button {
+    display: none;
+    margin: 0 auto;
+    color: #fff;
+    width: 160px;
+    height: 50px;
+    line-height: 50px;
+    background: #446CB3;
+    font-size: 22px;
+    border: 0;
+    border-radius: 3px;
+    box-shadow: 0 5px 5px -5px #333;
+    transition: background 0.3s ease-in;
+  }
+  
+  .modal-container .modal-backdrop {
+    height: 0;
+    width: 0;
+    opacity: 0;
+    overflow: hidden;
+    transition: opacity 0.2s ease-in;
+  }
+  
+  .modal-container #modal-toggle {
+    position: absolute;
+    left: 0;
+    top: 0;
+    height: 100%;
     width: 100%;
+    margin: 0;
+    opacity: 0;
+    cursor: pointer;
+  }
+  
+  .modal-container #modal-toggle:hover ~ button {
+    background: #1E824C;
+  }
+  
+  .modal-container #modal-toggle:checked {
+    width: 100vw;
+    height: 100vh;
+    position: fixed;
+    left: 0;
+    top: 0;
+    z-index: 9;
+    opacity: 0;
+  }
+  
+  .modal-container #modal-toggle:checked ~ .modal-backdrop {
+    background-color: rgba(0, 0, 0, 0.6);
+    width: 100vw;
+    height: 100vh;
+    position: fixed;
+    left: 0;
+    top: 0;
+    z-index: 9;
+    pointer-events: none;
+    opacity: 1;
+  }
+  
+  .modal-container #modal-toggle:checked ~ .modal-backdrop .modal-content {
+    background-color: #fff;
+    max-width: 400px;
+    width: 100%;
+    height: 280px;
+    padding: 10px 30px;
+    position: absolute;
+    left: calc(50% - 200px);
+    top: 12%;
+    border-radius: 4px;
+    z-index: 999;
+    pointer-events: auto;
+    cursor: auto;
+    box-shadow: 0 3px 7px rgba(0, 0, 0, 0.6);
+  }
+  @media (max-width: 400px) {
+    .modal-container #modal-toggle:checked ~ .modal-backdrop .modal-content {
+      left: 0;
+    }
+  }
+  
+  .modal-container
+    #modal-toggle:checked
+    ~ .modal-backdrop
+    .modal-content
+    .modal-close {
+    color: #666;
+    position: absolute;
+    right: 2px;
+    top: 0;
+    padding-top: 7px;
+    background: #fff;
+    font-size: 16px;
+    width: 25px;
+    height: 28px;
+    font-weight: bold;
+    text-align: center;
+    cursor: pointer;
+  }
+  
+  .modal-container
+    #modal-toggle:checked
+    ~ .modal-backdrop
+    .modal-content
+    .modal-close.button {
+    top: initial;
+    bottom: 20px;
+    right: 20px;
+    background: #4CAF50;
+    color: #fff;
+    width: 50px;
+    border-radius: 2px;
+    font-size: 14px;
+    font-weight: normal;
+  }
+  
+  .modal-container
+    #modal-toggle:checked
+    ~ .modal-backdrop
+    .modal-content
+    .modal-close.button:hover {
+    color: #fff;
+    background: #1E824C;
+  }
+  
+  .modal-container
+    #modal-toggle:checked
+    ~ .modal-backdrop
+    .modal-content
+    .modal-close:hover {
+    color: #333;
+  }
+  
+  #snap {
+    border: none;
+    margin: none;
+    padding: none;
+    position: absolute;
+    top: 50px;
+    left: 0px;
+  }
+  
+  #jitsi {
+    border: none;
+    margin: none;
+    padding: none;
+    position: fixed;
+    top: 50px;
+    left: 100vw;
+    min-width: 526px;
+    background-color: #333;
+  }
+  
+  .jitsiOpen {
+    animation-name: jitsiopenAnimation;
+    animation-duration:0.5s;
+    animation-iteration-count: 1;
+    animation-timing-function: ease;
+    animation-fill-mode: forwards;
+  }
+  
+  .jitsiClose {
+    animation-name: jitsicloseAnimation;
+    animation-duration:0.6s;
+    animation-iteration-count: 1;
+    animation-timing-function: ease;
+    animation-fill-mode: forwards;
+  }
+  
+  .jitsiHalf {
+    animation-name: jitsihalfAnimation;
+    animation-duration:0.5s;
+    animation-iteration-count: 1;
+    animation-timing-function: ease;
+    animation-fill-mode: forwards;
+  }
+  
+  @keyframes jitsiopenAnimation {
+    from {
+      left: 100%;
+      width: 0%;
+    }
+    to {
+      left: 0%;
+      width: 100%;
+    }  
+  }
+  
+  @keyframes jitsihalfAnimation {
+    from {
+      left: 0%;
+      width: 100%;
+      
+    }
+    to {
+      left: 50%;
+      width: 50%;
+    }  
+  }
+  
+  @keyframes jitsicloseAnimation {
+    from {
+      left: 50%;
+      width: 50%;
+      
+    }
+    to {
+      left: 100%;
+      width: 100%;
+    }  
+  }
+  
+  .snapOpen {
+    animation-name: snapopenAnimation;
+    animation-duration:0.5s;
+    animation-iteration-count: 1;
+    animation-timing-function: ease;
+    animation-fill-mode: forwards;
     
   }
-  to {
-    left: 0%;
-    width: 0%;
-  }  
-}
-
-@keyframes snaphalfAnimation {
-  from {
-    left: 0%;
-    width: 100%;
-    
+  
+  .snapClose {
+    animation-name: snapcloseAnimation;
+    animation-duration:1s;
+    animation-iteration-count: 1;
+    animation-timing-function: ease;
+    animation-fill-mode: forwards;
   }
-  to {
-    left: 0%;
-    width: 50%;
-  }  
-}
-
-@keyframes snapopenAnimation {
-  from {
-    left: 0%;
-    width: 50%;
+  
+  .snapHalf {
+    animation-name: snaphalfAnimation;
+    animation-duration:1s;
+    animation-iteration-count: 1;
+    animation-timing-function: ease;
+    animation-fill-mode: forwards;
   }
-  to {
-    left: 0%;
-    width: 100%;
-  }  
-}
-
-
-
-#open {
-  position: fixed;
-  top: 5px;
-  right: 1vw;
-  width: 250px;
-  z-index: 99999;
-}
-
-.dot {
-  height: 28px;
-  width: 28px;
-  background-color: #bbb;
-  border-radius: 50%;
-  display: inline-block;
-  margin-left: 25px;
-  border: 3px solid #F1F1F1;
-}
-
-.dot:hover {
-  background-color: #bbb;
-}
-
-#content {
-  min-width: 945px; /* Suppose you want minimum width of 1000px */
-  width: auto !important; /* Firefox will set width as auto */
-}
+  
+  @keyframes snapcloseAnimation {
+    from {
+      left: 0%;
+      width: 100%;
+      
+    }
+    to {
+      left: 0%;
+      width: 0%;
+    }  
+  }
+  
+  @keyframes snaphalfAnimation {
+    from {
+      left: 0%;
+      width: 100%;
+      
+    }
+    to {
+      left: 0%;
+      width: 50%;
+    }  
+  }
+  
+  @keyframes snapopenAnimation {
+    from {
+      left: 0%;
+      width: 50%;
+    }
+    to {
+      left: 0%;
+      width: 100%;
+    }  
+  }
+  
+  
+  
+  #open {
+    position: fixed;
+    top: 5px;
+    right: 1vw;
+    z-index: 99999;
+  }
+  
+  .dot {
+    height: 28px;
+    width: 28px;
+    background-color: #bbb;
+    border-radius: 50%;
+    display: inline-block;
+    margin-left: 25px;
+    border: 3px solid #F1F1F1;
+  }
+  
+  .dot:hover {
+    background-color: #bbb;
+  }
+  
+  #content {
+    width: auto !important; /* Firefox will set width as auto */
+  }
 </style>
--- src/pages/meeting/index.vue
+++ src/pages/meeting/index.vue
@@ -112,7 +112,7 @@ export default {
       app: "",
       config: {
         meetingSettings:
-          "#jitsi_meet_external_api_id=0&amp;config.requireDisplayName=true&amp;config.startAudioMuted=6&amp;config.disableAudioLevels=true&amp;interfaceConfig.DISABLE_VIDEO_BACKGROUND=true",
+          "#jitsi_meet_external_api_id=0&amp;config.requireDisplayName=true&amp;config.startAudioMuted=6&amp;config.disableAudioLevels=true&amp;interfaceConfig.DISABLE_VIDEO_BACKGROUND=true&interfaceConfig.SHOW_CHROME_EXTENSION_BANNER=false&config.disableDeepLinking=true",
       },
     };
   },
@@ -171,7 +171,6 @@ a:hover {
   left: 0px;
   height: 50px;
   width: 100%;
-  min-width: 945px;
   background: #212533;
   padding: 0;
   z-index: 10000;
@@ -366,7 +365,6 @@ h1 {
   /* position: fixed; */
   top: 5px;
   right: 1vw;
-  /* width: 250px; */
   z-index: 99999;
   display: flex;
 }
@@ -402,8 +400,4 @@ h1 {
   margin-top: 3px;
 }
 
-#content {
-  min-width: 945px; /* Suppose you want minimum width of 1000px */
-  width: auto !important; /* Firefox will set width as auto */
-}
 </style>
