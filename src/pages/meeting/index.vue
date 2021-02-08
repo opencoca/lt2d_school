4a7eeccb96d06d73f25965fb97ace32ffb24b5fa
OpenCo <admin@openco.ca>
2021-02-08 11:40:36 -0600

    Search Menu (#17) (#21)
    * Started search bar
    
    * Finished Search Bar
    
    * Added 'Please Rotate your device' for mobile users.
    
    * Fixed scrolling issue
    
    Co-authored-by: shadowlerone <andrae.lewis@gmail.com>

--- src/pages/meeting/index.vue
+++ src/pages/meeting/index.vue
@@ -90,7 +90,14 @@
           </a>
         </div>
       </div>
+      <div id="myModal" class="modal">
 
+      <!-- Modal content -->
+      <div class="modal-content">
+        <p class="rotate">Please rotate your device.</p>
+      </div>
+
+      </div>
       <div class="window">
         <iframe
           allow="microphone; camera"
@@ -428,5 +435,39 @@
     height: 18px;
     /* vertical-align:baseline; */
     margin-top: 3px;
+  }
+  /* The Modal (background) */
+  .modal {
+    display: none; /* Hidden by default */
+    position: fixed; /* Stay in place */
+    z-index: 1; /* Sit on top */
+    padding-top: 100px; /* Location of the box */
+    padding-bottom: 100px; /* Location of the box */
+    left: 0;
+    top: 0;
+    width: 100%; /* Full width */
+    height: 100%; /* Full height */
+    overflow: auto; /* Enable scroll if needed */
+    background-color: rgb(0,0,0); /* Fallback color */
+    background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+  }
+
+  /* Modal Content */
+  .modal-content {
+    background-color: #fefefe;
+    margin: auto;
+    padding: 20px;
+    border: 1px solid #888;
+    width: 80%;
+    height: 80%;
+    font-size: 3rem;
+  }
+  .rotate {
+    transform: rotate(-90deg) translate(-100%, 0%);
+  }
+  @media screen and (orientation:portrait) {
+    .modal {
+      display: block;
+    }
   }
 </style>
