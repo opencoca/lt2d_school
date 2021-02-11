a5af588385712219957bf0c73fdd6356e7855ec9
shadowlerone <andrae.lewis@gmail.com>
2021-02-11 01:43:13 -0500

    Prepped router for QRCode and App routes

--- src/router.js
+++ src/router.js
@@ -5,19 +5,34 @@ Vue.use(Router);
 
 const routes = [
   {
+    path: "/meet/:meet/app",
+    props: true,
+    component: () =>
+      import("./pages/app")
+  },
+  {
+    path: "/meet/:meet/qr",
+    props: true,
+    component: () =>
+      import("./pages/qrcode")
+  },
+  {
+    path: "/meet/:meet",
+    name: "meet",
+    props: true,
+    component: () =>
+      import(/* webpackChunkName: "page-meet" */ "./pages/meeting"),
+    children: [
+    ]
+  },
+  {
     path: "/",
     name: "Lobby",
     props: true,
     redirect: "/meet/Preville-Online-Lobby/"
 //    component: () => import(/* webpackChunkName: "page-meet" */ "./pages/meeting")
   },
-  {
-    path: "/meet/:meet/",
-    name: "meet",
-    props: true,
-    component: () =>
-      import(/* webpackChunkName: "page-meet" */ "./pages/meeting")
-  },
+  
   {
     path: "/about",
     name: "about",
